31af6c598cfd649f62e0ecc4be353ee55e5adda1
shiv-paka <shivapaka14@gmail.com>
2025-09-13 09:24:20 +0530

    Add files via upload

---
+++ experiment.py
@@ -0,0 +1,75 @@
+#vault
+"""print("Welcome to the vault!")
+a=input("Enter your username\n")
+c=["Shiva","Shri"]
+if a in c :
+   b=input("Welcome! , enter your password to proceed\n")
+   
+   if b=="Hello World":
+      print("Welcome to the vault")
+   else:
+      print("please check your password")
+
+else:
+   print("please create an account and try again")
+"""
+#f string
+"""a=input("Enter your name")
+name=f"Hello {a}"
+print(name)"""
+#Even or odd calculator
+"""print("Even or odd calculator")
+a=int(input("Enter a number\n"))
+b=a%2
+if b==0:
+   print("It's an even number")
+else:
+   print("It's an odd number")
+c=a%4
+if c==0:
+   print("your number is divisible by 4")
+else :
+   print("Your number isn't divisible by 4")"""
+
+"""a=10
+b=a
+a+=5
+print(a)"""
+#Minor calculator
+"""print("IDk what to name this calculator")
+a=int(input("Enter your age"))
+
+if a<18:
+    print("you're a minor")
+
+elif a>=100:
+    print("You're supposed to be 6 feet underground (respectfully)")
+
+else:
+    print("you're not a minor")"""
+
+"""print("Backwards word printer")
+a=input("Enter the word here\n")
+print(a[::-1])""" #chatgpt it
+
+#palindrome checker
+'''print('This is a palindrome checker. (A palindrome is a word that is spelt the same both ways)')
+palin=input('Enter a word:\n').lower()
+if palin==palin[::-1]:
+    print('The word is a palindrome.')
+else:
+    print('It is not a palindrome.')'''
+#Anagram checker
+'''wrd1=input('Enter a word:\n')
+wrd2=input('Enter another word:\n')
+if sorted(wrd1)==sorted(wrd2):
+        print('Those are anagrams.')
+else:
+    print('They are not anagrams.')'''
+     
+'''if wrd1==wrd2:
+    print('those words are anagrams.')
+else:
+    print('those are not anagrams.')'''
+
+
