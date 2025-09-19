296ddb5407f4294c189f8edbe340d2cae7a1817b
shiv-paka <shivapaka14@gmail.com>
2025-09-19 06:45:38 +0530

    hangman.py

---
+++ hangman.py
@@ -0,0 +1,13 @@
+import random
+print("Hangman?")
+wrds=["hello","hi","hola","bonjour","annyeonghaseyo"]
+ranPick=random.choice(wrds)
+for i in range(len(ranPick)):
+    print("",end=" _ ")
+print()
+for i in range(len(ranPick)):
+    wrdInp=input('Guess a letter: ').lower().strip()
+    if wrdInp in ranPick:
+        print(wrdInp)
+    else :
+        print(f"Try again only {i-1} guesses left!")
